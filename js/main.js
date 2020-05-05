3a18570f103a06f3c319220c07fe2ee128ba8806
Filya70 <f0rest70@mail.ru>
2020-05-05 14:37:48 +0300

    Day 1

--- js/main.js
+++ js/main.js
@@ -8,3 +8,78 @@ close.addEventListener("click", toggleModal);
 function toggleModal() {
   modal.classList.toggle("is-open");
 }
+
+
+const buttonAuth = document.querySelector('.button-auth')
+const modalAuth = document.querySelector('.modal-auth')
+const closeAuth = document.querySelector('.close-auth')
+const logInForm = document.getElementById('logInForm')
+const logInInput = document.getElementById('login')
+const logInPassword = document.getElementById('password')
+const userName = document.querySelector('.user-name')
+const buttonOut = document.querySelector('.button-out')
+
+
+let login = localStorage.getItem('login')
+
+
+
+function toggleModalAuth () {
+  modalAuth.classList.toggle("is-open");
+}
+
+function authorized () {
+  console.log('Authorized');
+
+  function logOut (){
+    login = ''
+    buttonAuth.style.display = ''
+    userName.style.display = ''
+    buttonOut.style.display = ''
+    buttonOut.removeEventListener('click', logOut)
+    localStorage.removeItem('login')
+    checkAuth()
+  }
+
+  buttonAuth.style.display = 'none'
+  userName.style.display = 'block'
+  buttonOut.style.display = 'block'
+  userName.textContent = login
+  buttonOut.addEventListener('click', logOut)
+}
+
+function notAuthorized () {
+  console.log('Not Authorized');
+
+  function logIn (e){
+    if(logInInput.value.length > 3){
+      e.preventDefault()
+      login = logInInput.value
+      localStorage.setItem('login', login)
+
+      logInForm.reset()
+      toggleModalAuth()
+      buttonAuth.removeEventListener('click', toggleModalAuth)
+      closeAuth.removeEventListener('click', toggleModalAuth)
+      logInForm.removeEventListener('submit', logIn)
+      checkAuth()
+    }else{
+      e.preventDefault()
+      alert('Логин должен содержать больше 3 символов')
+    }
+  }
+
+  buttonAuth.addEventListener('click', toggleModalAuth)
+  closeAuth.addEventListener('click', toggleModalAuth)
+  logInForm.addEventListener('submit', logIn)
+}
+
+function checkAuth () {
+  if(login){
+    authorized()
+  }else{
+    notAuthorized()
+  }
+}
+
+checkAuth()
